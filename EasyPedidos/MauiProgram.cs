69a9dc7b8c56e1743185317c4fc50f7328d81963
Robson Basseto <robsonrafaelfacul@gmail.com>
2025-11-12 22:31:46 -0300

    Instalação do SyncFusion e refatoração da tela principal e tela de pedido

--- EasyPedidos/MauiProgram.cs
+++ EasyPedidos/MauiProgram.cs
@@ -1,6 +1,7 @@
-﻿using EasyPedidos.ViewModels;
+﻿using CommunityToolkit.Maui;
 using EasyPedidos.Pages;
-using CommunityToolkit.Maui;
+using EasyPedidos.ViewModels;
+using Syncfusion.Maui.Core.Hosting; 
 
 namespace EasyPedidos
 {
@@ -12,26 +13,29 @@ namespace EasyPedidos
             builder
                 .UseMauiApp<App>()
                 .UseMauiCommunityToolkit()
+                .ConfigureSyncfusionCore()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
-            // ViewModels
+            // SYNC FUSION LICENÇA (MESMO FREE)
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JFaF1cX2hIf0x3THxbf1x1ZFBMZFlbRnJPIiBoS35Rc0RiWH1fd3BQRGRbU0ZwVEFc");
+
+            // REGISTRO DE VIEWMODELS E PAGES
             builder.Services.AddTransient<PedidoViewModel>();
             builder.Services.AddTransient<ListPedidoViewModel>();
             builder.Services.AddTransient<DetalhesPedidoViewModel>();
             builder.Services.AddTransient<EditarPedidoViewModel>();
             builder.Services.AddTransient<FinalizarPedidoViewModel>();
 
-            // Pages
             builder.Services.AddTransient<PedidoPage>();
             builder.Services.AddTransient<ListPedidoPage>();
             builder.Services.AddTransient<DetalhesPedidoPage>();
             builder.Services.AddTransient<EditarPedidoPage>();
             builder.Services.AddTransient<FinalizarPedidoPage>();
 
-            return builder.Build();
+            return builder.Build(); // ← SÓ UM BUILD
         }
     }
 }
